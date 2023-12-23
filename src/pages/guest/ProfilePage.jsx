249a2d023e91f3f02598bb6e79af53605c71eead
Frangeul Nathan <seekerz17@outlook.fr>
2023-12-23 16:10:10 +0100

    Did fetch get all on ProfilePage

--- src/pages/guest/ProfilePage.jsx
+++ src/pages/guest/ProfilePage.jsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
+
 function ProfilePage() {
-    return (
-      <>
-      </>
-    );
-  }
-  
-  export default ProfilePage;
-  
+
+  const [profiles, setProfiles] = useState(null);
+
+  useEffect(() => {
+
+    (async () => {
+      const reponse = await fetch(`http://localhost:3001/api/profils/`)
+      const data = await reponse.json();
+      setProfiles(data)
+      console.log(data);
+    })()
+
+  }, [])
+
+ 
+  return (
+    <>
+      <main>
+        {/* Est-ce que cocktail contient quelque chose ? */}
+        {profiles ? (
+          <>
+            {/* Je fais un array map pour récupérer les cocktails grâce à la variable cocktails de useState */}
+            {profiles.map((profile) => {
+              return (
+                <article>
+                  <h2> Pseudo : {profile.inGameName} </h2>
+                  <p> Bio : {profile.profilBio} </p>
+                  {/* Ternaire pour que REACT me retourne mon composant même si leur valeur sont null */}
+                  <p> Rang : {profile.GameRank?.ranksLabel || "N/A"} </p>
+                  <p> Rôle : {profile.GameRole?.gameRoleLabel || "N/A"} </p>
+                </article>
+              )
+
+            })}
+          </>
+
+        ) : (
+
+          <p>Profils en cours de chargement</p>
+
+        )}
+      </main>
+    </>
+  );
+}
+
+export default ProfilePage;
